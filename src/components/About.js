cc794d8cae9ea586ea0376d92486f865e108d150
benja7 <benjaminmutava@gmail.com>
2023-01-17 23:03:18 +0300

    assignment completed

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+const About = (props) => {
+    return (
+      <div>
+          <aside>
+  
+              <img src= {props.image ||  "https://via.placeholder.com/215" }  alt="blog logo"></img>
+  
+              <p>{props.about}</p>
+  
+          </aside>
+          
+      </div>
+    )
+  }
+  
+  export default About
